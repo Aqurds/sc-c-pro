5b7990a3d0ba6e408d446a87b629fc24641ba87f
aqurds <omarf1320@gmail.com>
2023-06-23 17:50:14 +0600

    Add Sanity

---
+++ connect_colleague_backend/schemas/index.ts
@@ -0,0 +1,7 @@
+import user from './user';
+import save from './save';
+import comment from './comment';
+import pin from './pin';
+import postedBy from './postedBy';
+
+export const schemaTypes = [user, save, comment, pin, postedBy]
